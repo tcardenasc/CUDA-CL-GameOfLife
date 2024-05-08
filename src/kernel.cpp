a83154c0d2865cb23b7eb707e3592b3a549688a1
Tomás Cárdenas <tomas.cardenas@ug.uchile.cl>
2024-05-08 14:25:49 -0400

    basic constructor and destructor for Cpu implementation

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -1,7 +1,19 @@
 #include "kernel.h"
 
-void vec_sum(int* a, int* b, int* c, int n) {
-  for (int i = 0; i < n; i++) {
-    c[i] = a[i] + b[i];
-  }
+CpuLife::CpuLife() :
+        m_data(nullptr),
+        m_resultData(nullptr),
+        m_worldWidth(0),
+        m_worldHeight(0),
+        m_dataLength(0),
+        m_randomGen(std::random_device()()) {}
+
+CpuLife::~CpuLife() {
+    delete[] m_data;
+    m_data = nullptr;
+    delete[] m_resultData;
+    m_resultData = nullptr;
+    m_dataLength = 0;
 }
+
+
--- src/kernel.h
+++ src/kernel.h
@@ -1,3 +1,24 @@
+#include <random>
 
+typedef unsigned char ubyte;
 
-void vec_sum(int* a, int* b, int* c, int n);
+class CpuLife {
+protected:
+    // Naming convention: m_ for member variables
+    // World data
+    ubyte *m_data;
+    // Helper buffer
+    ubyte *m_resultData;
+
+    // World size
+    size_t m_worldWidth;
+    size_t m_worldHeight;
+    size_t m_dataLength; // m_worldWidth * m_worldHeight
+
+    // Random number generator
+    std::mt19937 m_randomGen;
+
+public:
+    CpuLife();
+    ~CpuLife();
+};
