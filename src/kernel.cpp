7a35d2a7f930ee1eb35b638d5be92e6758a41f54
Tomás Cárdenas <tomas.cardenas@ug.uchile.cl>
2024-05-09 15:45:13 -0400

    CPU basic implementation

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -9,11 +9,54 @@ CpuLife::CpuLife() :
         m_randomGen(std::random_device()()) {}
 
 CpuLife::~CpuLife() {
+    freeBuffers();
+}
+
+void CpuLife::freeBuffers() {
     delete[] m_data;
     m_data = nullptr;
     delete[] m_resultData;
     m_resultData = nullptr;
     m_dataLength = 0;
 }
+
+void CpuLife::allocBuffers() {
+    m_dataLength = m_worldWidth * m_worldHeight;
+    m_data = new ubyte[m_dataLength];
+    m_resultData = new ubyte[m_dataLength];
+}
+
+void CpuLife::resize(size_t width, size_t height) {
+    freeBuffers();
+    m_worldWidth = width;
+    m_worldHeight = height;
+}
+
+void CpuLife::init() {
+    for (size_t i = 0; i < m_dataLength; ++i) {
+        m_data[i] = m_randomGen() & 1;
+    }
+}
+
+void CpuLife::iterateSerial(size_t iterations) {
+    for (size_t i = 0; i < iterations; ++i) {
+        for (size_t y = 0; y < m_worldHeight; ++y) {
+            size_t y0 = ((y + m_worldHeight - 1) % m_worldHeight) * m_worldWidth;
+            size_t y1 = y * m_worldWidth;
+            size_t y2 = ((y + 1) % m_worldHeight) * m_worldWidth;
+
+            for (size_t x = 0; x < m_worldWidth; ++x) {
+                size_t x0 = (x + m_worldWidth - 1) % m_worldWidth;
+                size_t x2 = (x + 1) % m_worldWidth;
+
+                ubyte aliveCells = countAliveCells(m_data, x0, x, x2, y0, y1, y2);
+                m_resultData[y1 + x] = aliveCells == 3 || (aliveCells == 2 && m_data[x + y1]) ? 1 : 0;
+            }
+        }
+
+        std::swap(m_data, m_resultData);
+    }
+}
+
 
 
--- src/kernel.h
+++ src/kernel.h
@@ -3,6 +3,13 @@
 typedef unsigned char ubyte;
 
 class CpuLife {
+private:
+    static inline ubyte countAliveCells(ubyte *data, size_t x0, size_t x1, size_t x2, size_t y0, size_t y1, size_t y2) {
+        return data[x0 + y0] + data[x1 + y0] + data[x2 + y0]
+               + data[x0 + y1] + data[x2 + y1]
+               + data[x0 + y2] + data[x1 + y2] + data[x2 + y2];
+    }
+
 protected:
     // Naming convention: m_ for member variables
     // World data
@@ -20,5 +27,16 @@ protected:
 
 public:
     CpuLife();
+
     ~CpuLife();
+
+    void freeBuffers();
+
+    void allocBuffers();
+
+    void resize(size_t width, size_t height);
+
+    void init();
+
+    void iterateSerial(size_t iterations);
 };
